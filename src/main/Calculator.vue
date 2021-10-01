0c64e1732bdfa6887b1809f251f85a7e56d37986
Polyane Tuag <pollyportugues@hotmail.com>
2021-10-01 10:05:15 -0300

    creating main Calculator

---
+++ src/main/Calculator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="calculator">
+    <Display />
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from "../components/Button.vue"
+import Display from "../components/Display.vue"
+
+export default {
+  components: { Button, Display }
+}
+</script>
+
+<style>
+  .calculator {
+    height: 320px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+  }
+</style>
