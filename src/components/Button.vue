d433fbb2dd8fa36556993a370ca6a7263db48fd9
Polyane Tuag <pollyportugues@hotmail.com>
2021-10-01 10:25:32 -0300

    implementing component Button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,15 +1,54 @@
 <template>
-  <button class="button">
-    0
+  <button class="button" :class="{ double, triple, operation }">
+    
+    {{ label }}
   </button>
 </template>
 
 <script>
 export default {
-
+  props: {
+    label: {},
+    operation: {type: Boolean },
+    double: {type: Boolean },
+    triple: {type: Boolean }
+  }
 }
 </script>
 
 <style>
+  :root {
+    --bg-button: #f0f0f0;
+    --border-button: solid 1px #888;
+  }
 
+  .button {
+    font-size: 1.4rem;
+    background: var(--bg-button);
+    border: none;
+    border-right: var(--border-button);
+    border-bottom: var(--border-button);
+    outline: none;
+  }
+
+  .button:active {
+    background: #ccc;
+  }
+
+  .button.double {
+    grid-column: span 2;
+  }
+
+  .button.triple {
+    grid-column: span 3;
+  }
+
+  .button.operation {
+    background: #fa8231;
+    color: #fff;
+  }
+
+  .button.operation:active {
+    background: #fa8231cc;
+  }
 </style>
